140f2660eb27880a406d50e49e4e99fbd4824ffa
Finn-Thorben Sell <dev@finn-thorben.me>
2022-09-03 18:43:01 +0200

    make vite build place files into vite_dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import * as path from "path";
 import { defineConfig } from "vite";
 import Inspect from "vite-plugin-inspect";
 import RuntimeConfig from "./src";
 
 export default defineConfig({
     plugins: [Inspect(), RuntimeConfig()],
+    build: {
+        outDir: path.resolve("./vite_dist"),
+    },
 });
