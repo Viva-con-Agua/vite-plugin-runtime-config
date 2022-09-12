17b5b97ce7329c4b86a1493d8a100b7cc9aa49ef
Finn-Thorben Sell <dev@finn-thorben.me>
2022-09-12 16:30:35 +0200

    add vue to test project

--- test_project/vite.config.ts
+++ test_project/vite.config.ts
@@ -1,9 +1,10 @@
 import * as path from "path";
 import { defineConfig } from "vite";
-import RuntimeConfig from "vite-plugin-runtime-config";
+import runtimeConfig from "vite-plugin-runtime-config";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    plugins: [RuntimeConfig()],
+    plugins: [vue(), runtimeConfig()],
     build: {
         outDir: path.resolve("./vite_dist"),
     },
